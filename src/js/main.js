dd05f6b06e4c996f8faa2af16a3790ab582c6784
Karolina Kulinska <karolina.kulinska89@gmail.com>
2020-02-18 11:40:40 +0100

    feat: add scroll top button #topBtn

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,28 @@
 "use strict";
 
-document.querySelectorAll('.navigation__list a.navigation__link').forEach(function(element) {
-    element.addEventListener('click', function() {
+document.querySelectorAll('.navigation__list a.navigation__link').forEach(function (element) {
+    element.addEventListener('click', function () {
         document.querySelector('#navi-toggle').checked = false;
-    });    
+    });
 });
+
+
+$(document).ready(function () {
+
+
+$(window).scroll(function(){
+    if($(this).scrollTop() > 40){
+        $('#topBtn').fadeIn();
+    } else {
+        $('#topBtn').fadeOut();
+    }
+})
+
+    $("#topBtn").click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+
+    });
+
+})
